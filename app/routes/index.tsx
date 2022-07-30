aa49c429d8d10d902ba2d2b4baca7a91a03bd81b
Sophia <sophialealbarros@gmail.com>
2022-07-30 20:50:39 -0300

    add styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,27 @@
+import type { LinksFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
+import styles from '~/styles/index.css';
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
+
 const Home = () => {
   return (
-    <h1>Hello, World!</h1>
+    <div className='container'>
+      <div className='content'>
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to='jokes'>Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
 
---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,10 @@
+const Joke = () => {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>A joke will appear here</p>
+    </div>
+  );
+};
+
+export default Joke;
