f72b3919d294cd0f1b59b375b041061a6b953777
Sophia <sophialealbarros@gmail.com>
2022-07-30 22:49:41 -0300

    validation

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,25 +1,72 @@
-import { ActionFunction, redirect } from '@remix-run/node';
-import { Form } from '@remix-run/react';
+import { json, redirect } from '@remix-run/node';
+import type { ActionFunction } from '@remix-run/node';
+import { Form, useActionData } from '@remix-run/react';
 import { db } from '~/utils/db.server';
+
+const validateJokeName = (name: string) => {
+  if (name.length < 3) {
+    return 'This name is too short!';
+  }
+};
+
+const validateJokeContent = (content: string) => {
+  if (content.length < 10) {
+    return 'This joke is too short!';
+  }
+};
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    name: string | undefined;
+    content: string | undefined;
+  };
+  fields?: {
+    name: string;
+    content: string;
+  };
+};
+
+const badRequest = (data: ActionData) => {
+  return json(data, { status: 400 });
+};
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
 
-  const name = form.get('name');
-  const content = form.get('content');
+  let name = form.get('name');
+  let content = form.get('content');
 
   if (typeof name !== 'string' || typeof content !== 'string') {
-    throw new Error(`Form not submitted correctly.`);
+    return badRequest({
+      formError: 'Form not submitted correctly.',
+    });
+  }
+
+  name = name.replace(/\s+/g, ' ');
+  content = content.replace(/\s+/g, ' ');
+
+  const fieldErrors = {
+    name: validateJokeName(name),
+    content: validateJokeContent(content),
+  };
+
+  const fields = { name, content };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields });
   }
 
   const joke = await db.joke.create({
-    data: { name, content },
+    data: fields,
   });
 
   return redirect(`/jokes/${joke.id}`);
 };
 
 const NewJoke = () => {
+  const actionData = useActionData<ActionData>();
+
   return (
     <div>
       <main>
@@ -28,16 +75,54 @@ const NewJoke = () => {
           <div>
             <label htmlFor='name'>
               Name:
-              <input type='text' name='name' />
+              <input
+                type='text'
+                defaultValue={actionData?.fields?.name}
+                name='name'
+                aria-invalid={
+                  Boolean(actionData?.fieldErrors?.name) || undefined
+                }
+                aria-errormessage={
+                  actionData?.fieldErrors?.name ? 'name-error' : undefined
+                }
+              />
             </label>
+            {actionData?.fieldErrors?.name ? (
+              <p className='form-validation-error' role='alert' id='name-error'>
+                {actionData.fieldErrors.name}
+              </p>
+            ) : null}
           </div>
           <div>
             <label htmlFor='content'>
               Content:
-              <textarea name='content' />
+              <textarea
+                name='content'
+                defaultValue={actionData?.fields?.content}
+                aria-invalid={
+                  Boolean(actionData?.fieldErrors?.content) || undefined
+                }
+                aria-errormessage={
+                  actionData?.fieldErrors?.content ? 'content-error' : undefined
+                }
+              />
             </label>
+            {actionData?.fieldErrors?.content ? (
+              <p
+                className='form-validation-error'
+                role='alert'
+                id='content-error'
+              >
+                {actionData.fieldErrors.content}
+              </p>
+            ) : null}
           </div>
           <div>
+            {actionData?.formError ? (
+              <p className='form-validation-error' role='alert'>
+                {actionData.formError}
+              </p>
+            ) : null}
             <button type='submit' className='button'>
               Add joke
             </button>
