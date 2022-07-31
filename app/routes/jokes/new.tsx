b5ceaf15854bb39200c9b58daa5961d252c1bcb9
Sophia <sophialealbarros@gmail.com>
2022-07-30 22:26:45 -0300

    add new joke

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,4 +1,23 @@
+import { ActionFunction, redirect } from '@remix-run/node';
 import { Form } from '@remix-run/react';
+import { db } from '~/utils/db.server';
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+
+  const name = form.get('name');
+  const content = form.get('content');
+
+  if (typeof name !== 'string' || typeof content !== 'string') {
+    throw new Error(`Form not submitted correctly.`);
+  }
+
+  const joke = await db.joke.create({
+    data: { name, content },
+  });
+
+  return redirect(`/jokes/${joke.id}`);
+};
 
 const NewJoke = () => {
   return (
